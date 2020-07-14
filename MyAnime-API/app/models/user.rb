10dea11bd3d4b1ebca7b2288aedd0abce5c04e49
Caleb Kavanaugh <calebck@hotmail.com>
2020-07-14 15:53:24 -0400

    Added JWT for User authentication in Rails API (MyAnime-API).

--- MyAnime-API/app/models/user.rb
+++ MyAnime-API/app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+
     has_many :user_schedules
     has_many :user_anime_reviews
     has_many :animes, through: :user_schedules
-
-    # validates :name, presence: true
-    # validates :email, uniqueness: true
-    # has_secure_password
 end
--- MyAnime-API/config/routes.rb
+++ MyAnime-API/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   
+  resource :users, only: [:create]
+   post "/login", to: "auth#login"
+   get "/auto_login", to: "auth#auto_login"
+   get "/user_is_authed", to: "auth#user_is_authed"
+   
   scope '/api' do
     resources :animes
   end
